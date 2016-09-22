b471a002c6b5495f4e780cb9ea0786048f03a854
Peter Andersson <pa222ku@student.lnu.se>
2016-09-22 19:07:53 +0200

    Add input/view prototype

---
+++ prototyp/vy/vy/Model.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace vy
+{
+    class Model
+    {
+        public void CreateMember()
+        {
+            Console.WriteLine("CreateMember was called!");
+        }
+        public void RetrieveMember()
+        {
+            Console.WriteLine("RetrieveMember was called!");
+        }
+        public void UpdateMember()
+        {
+            Console.WriteLine("UpdateMember was called!");
+        }
+        public void DeleteMember()
+        {
+            Console.WriteLine("DeleteMember was called!");
+        }
+        public void CreateBoat()
+        {
+            Console.WriteLine("CreateBoat was called!");
+        }
+        public void RetrieveBoat()
+        {
+            Console.WriteLine("RetrieveBoat was called!");
+        }
+        public void UpdateBoat()
+        {
+            Console.WriteLine("UpdateBoat was called!");
+        }
+        public void ListMembersCL()
+        {
+            Console.WriteLine("ListMembersCL was called!");
+        }
+        public void ListMembersVL()
+        {
+            Console.WriteLine("ListMembersVL was called!");
+        }
+    }
+}
