5febdf057494796ce03384d4b1946b9829fe1975
Peter Andersson <pa222ku@student.lnu.se>
2016-09-23 08:24:54 +0200

    Add 'vy'

---
+++ vy/vy/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace vy
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var model = new Model();
+            var view = new View(model);
+            view.Loop();
+        }
+    }
+}
