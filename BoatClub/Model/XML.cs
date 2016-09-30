3bad3b2b3ed2b0159fed2cfde4b9e5d83217d8e8
Christian Trosell <ct222hv@student.lnu.se>
2016-09-30 15:51:30 +0200

    Add method for saving a list of members to xml file plus get a list of members from xml file.

--- BoatClub/Model/XML.cs
+++ BoatClub/Model/XML.cs
@@ -1,13 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
 
 namespace BoatClub.Model
 {
     class XML
     {
-        public static void save(String data)
+        private static string MemberListXMLPath = "../../Storage/membersList.xml";
+
+        public static void SaveMemberListToXMLFile(List<Member> memberList)
         {
-            // Save data
+            XmlSerializer writer = new XmlSerializer(typeof(List<Member>));
+            FileStream file = File.Create(MemberListXMLPath);
+            writer.Serialize(file, memberList);
+            file.Close();
         }
+
+        public static List<Member> GetMemberListFromXMLFile()
+        {
+            XmlSerializer reader = new XmlSerializer(typeof(List<Member>));
+            StreamReader file = new StreamReader(MemberListXMLPath);
+            List<Member> memberList = (List<Member>)reader.Deserialize(file);
+            file.Close();
+
+            return memberList;
+        }
+
         public static String load()
         {
             // Load data
