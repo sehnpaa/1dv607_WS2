e117555551e4d952cea79aac4cbb03d7b64e3ceb
Christian Trosell <ct222hv@student.lnu.se>
2016-09-30 16:19:06 +0200

    Add two types that fit the format for the diferent lists (verbose/compact)

---
+++ BoatClub/Model/MemberListCompactItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BoatClub.Model
+{
+    class MemberListCompactItem //Name, member id, number of boats
+    {
+        public string Name;
+        public string MemberID;
+        public int NumberOfBoats;
+    }
+}
---
+++ BoatClub/Model/MemberListVerboseItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BoatClub.Model
+{
+    class MemberListVerboseItem //Name, personal number, member id, boats with information
+    {
+        public string Name;
+        public string MemberID;
+        public string PersonalNumber;
+        public List<Boat> Boats;
+    }
+}
