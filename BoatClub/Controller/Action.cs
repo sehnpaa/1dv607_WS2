b703008bb895f2c741b7fae9fab9d935d55c619b
Peter Andersson <pa222ku@student.lnu.se>
2016-10-03 09:59:21 +0200

    Add support for 'create_member' as user input

--- BoatClub/Controller/Action.cs
+++ BoatClub/Controller/Action.cs
@@ -8,18 +8,44 @@ namespace BoatClub.Controller
 {
     class Action
     {
-        private readonly string _input;
         private readonly MemberRegistry _registry;
+
+        private string _command;
+        private List<String> _args;
+
+        private readonly Dictionary<string, int> _validNumberOfArgs = new Dictionary<string, int>()
+        {
+            {"create_member", 1}
+        };
 
         public Action(string input, MemberRegistry registry)
         {
-            _input = input;
             _registry = registry;
+            SetCommand(input);
+            SetArgs(input);
         }
 
         public void Call()
         {
-            _registry.Register(_input);
+            switch (_command)
+            {
+                case "create_member":
+                    if (_validNumberOfArgs[_command] == _args.Count)
+                    {
+                        _registry.Register(_args[0]);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        private void SetCommand(String s)
+        {
+            _command = s.Trim().Split(' ').First();
+        }
+        private void SetArgs(String s)
+        {
+            _args = s.Trim().Split(' ').Skip(1).ToList();
         }
     }
 }
