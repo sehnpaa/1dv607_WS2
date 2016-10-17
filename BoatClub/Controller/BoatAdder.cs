5a74fd79d1234a92b6ec0574dcb26103280ceb6a
Peter Andersson <pa222ku@student.lnu.se>
2016-10-17 11:09:42 +0200

    Move instansiation

--- BoatClub/Controller/BoatAdder.cs
+++ BoatClub/Controller/BoatAdder.cs
@@ -14,9 +14,8 @@ namespace BoatClub.Controller
             var memberId = args[0];
             var boatTypeInput = args[1];
             var lengthInMetresInput = args[2];
-            var boat = CreateBoat(boatTypeInput, lengthInMetresInput);
 
-            registry.AddBoat(memberId, boat);
+            registry.AddBoat(memberId, boatTypeInput, lengthInMetresInput);
             _member = registry.GetMemberById(memberId);
         }
 
@@ -24,32 +23,6 @@ namespace BoatClub.Controller
         {
             cli.Display("Successfully added boat.");
             cli.DisplayMember(_member);
-        }
-
-        private Boat CreateBoat(string boatTypeInput, string lengthInMetresInput)
-        {
-            if (Enum.IsDefined(typeof(BoatType), boatTypeInput))
-            {
-                double lengthInMetres;
-                var boatType = (BoatType) Enum.Parse(typeof(BoatType), boatTypeInput);
-                double.TryParse(lengthInMetresInput, out lengthInMetres);
-                var boat = new Boat(boatType, lengthInMetres);
-
-                return boat;
-            }
-            var listOfValidBoatTypes = "";
-            var boatTypes = Enum.GetNames(typeof(BoatType));
-
-            for (int i = 0; i < boatTypes.Length; i++)
-            {
-                listOfValidBoatTypes += boatTypes[i];
-
-                if (i < boatTypes.Length - 1)
-                {
-                    listOfValidBoatTypes += ", ";
-                }
-            }
-            throw new Exception($"You have entered an invalid type of boat. \nVaild boat types: {listOfValidBoatTypes}");
         }
     }
 }
--- BoatClub/Model/MemberRegistry.cs
+++ BoatClub/Model/MemberRegistry.cs
@@ -23,6 +23,55 @@ namespace BoatClub.Model
             SaveMemberList(memberList);
         }
 
+        public void AddBoat(string memberId, string boatType, string lengthInMetres)
+        {
+            var boat = CreateBoat(boatType, lengthInMetres);
+            var memberList = GetMemberList();
+            var found = false;
+
+            foreach (var member in memberList)
+            {
+                if (member.MemberId == memberId)
+                {
+                    member.Boats.Add(boat);
+                    found = true;
+                    break;
+                }
+            }
+            if (!found)
+            {
+                throw new Exception("No member with that id.");
+            }
+
+            SaveMemberList(memberList);
+        }
+
+        private Boat CreateBoat(string boatTypeInput, string lengthInMetresInput)
+        {
+            if (Enum.IsDefined(typeof(BoatType), boatTypeInput))
+            {
+                double lengthInMetres;
+                var boatType = (BoatType)Enum.Parse(typeof(BoatType), boatTypeInput);
+                double.TryParse(lengthInMetresInput, out lengthInMetres);
+                var boat = new Boat(boatType, lengthInMetres);
+
+                return boat;
+            }
+            var listOfValidBoatTypes = "";
+            var boatTypes = Enum.GetNames(typeof(BoatType));
+
+            for (int i = 0; i < boatTypes.Length; i++)
+            {
+                listOfValidBoatTypes += boatTypes[i];
+
+                if (i < boatTypes.Length - 1)
+                {
+                    listOfValidBoatTypes += ", ";
+                }
+            }
+            throw new Exception($"You have entered an invalid type of boat. \nVaild boat types: {listOfValidBoatTypes}");
+        }
+
         public string GetNextMemberId()
         {
             int id;
